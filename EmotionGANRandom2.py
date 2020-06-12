7f5963c63e9345ce5cdcde60db0b93e908892451
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2020-06-12 15:30:10 +0200

    plotLosses

--- EmotionGANRandom2.py
+++ EmotionGANRandom2.py
@@ -22,9 +22,6 @@ def denormImage(img):
     img = (img + 1) * 127.5
     return img.astype(np.uint8)
 
-def generateNoise(batchSize, noiseShape):
-    return np.random.normal(0, 1, size=(batchSize,) + noiseShape)
-
 class EmotionGANRandom2():
     def __init__(self, noiseShape, imageShape):
         self.generator = self.generateGenerator(noiseShape)
@@ -105,7 +102,7 @@ class EmotionGANRandom2():
                 # NOTE load real images
                 realImagesX = self.getSamplesFromDataset(iBatch, iBatch + batchSize)[0]
                 # NOTE generate fake images with generator
-                noise = generateNoise(len(realImagesX), self.noiseShape)
+                noise = self.generateNoise(len(realImagesX))
                 fakeImagesX = self.generator.predict(noise)
                 # NOTE save generator samples
                 if epoch % 10 == 0 and iBatch == 0:
@@ -125,7 +122,7 @@ class EmotionGANRandom2():
                 averageDiscriminatorRealLoss.append(discriminatorMetricsReal[0])
                 averageDiscriminatorFakeLoss.append(discriminatorMetricsFake[0])
                 # NOTE train adversial model
-                ganX = generateNoise(len(realImagesX), self.noiseShape)
+                ganX = self.generateNoise(len(realImagesX))
                 ganY = realDataY
                 self.generator.trainable = True
                 self.discriminator.trainable = False
@@ -141,6 +138,10 @@ class EmotionGANRandom2():
                 print("Average Disc_real loss: %f" % (np.mean(averageDiscriminatorRealLoss)))
                 print("Average GAN loss: %f" % (np.mean(averageGanLoss)))
                 print("-----------------------------------------------------------------")
+        return {"Discriminator real": averageDiscriminatorRealLoss, "Discriminator fake": averageDiscriminatorFakeLoss, "Adversial": averageGanLoss}
+
+    def generateNoise(self, batchSize):
+        return np.random.normal(0, 1, size=(batchSize,) + self.noiseShape)
 
     def saveImageBatch(self, imageBatch, fileName):
         plt.figure(figsize=(4,4))
@@ -158,6 +159,7 @@ class EmotionGANRandom2():
             fig.axes.get_yaxis().set_visible(False)
         plt.tight_layout()
         plt.savefig(self.imageSaveDir + "/" + fileName, bbox_inches="tight", pad_inches=0)
+        plt.clf()
     
     def loadImage(self, imagesDir, fileName):
         image = PIL.Image.open(self.datasetDir + "/cropped_aligned/cropped_aligned/" + imagesDir + "/" + fileName)
@@ -183,14 +185,22 @@ class EmotionGANRandom2():
                 images += [self.loadImage(imagesDir, fileName) for fileName in imageFilesNames]
                 if len(lines) == countEnd - countStart: break
                 countSkippedLabels += len(linesToAdd)
-        return np.array(images), lines
+        return np.array(images), np.array(lines)
+
+def plotLosses(losses:dict):
+    for key, value in losses.items():
+        plt.plot(value, label=key)
+    plt.ylabel("loss")
+    plt.legend()
+    plt.show()
 
 NOISE_SHAPE = (1,1,100)
-EPOCHS = 1000
+EPOCHS = 500
 BATCH_SIZE = 64
 # IMAGE_SHAPE = (112,112,3)
 IMAGE_SHAPE = (64,64,3)
 
 if __name__ == "__main__":
     gan = EmotionGANRandom2(NOISE_SHAPE, IMAGE_SHAPE)
-    gan.fit(EPOCHS, BATCH_SIZE)
+    losses = gan.fit(EPOCHS, BATCH_SIZE)
+    plotLosses(losses)
